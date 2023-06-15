ff55d7bc1deb01bb71a3338580f89ee6d9aa6089
Ruslan <ya.ruslan2013@mail.ru>
2023-06-15 09:13:01 +0300

    Test bot

---
+++ app/app/Http/Controllers/Api/TestSdkController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\Api;
+
+class TestSdkController extends Controller
+{
+    protected $telegram;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @param  Api  $telegram
+     */
+    public function __construct(Api $telegram)
+    {
+        $this->telegram = $telegram;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // $response = $this->telegram->getMe();
+        // $botId = $response->getId();
+        // $firstName = $response->getFirstName();
+        // $username = $response->getUsername();
+
+        // $response = $this->telegram->sendMessage([
+        //     'chat_id' => 'CHAT_ID',
+        //     'text' => 'Hello World'
+        // ]);
+
+        // $messageId = $response->getMessageId();
+
+        $updates = $this->telegram->getUpdates();
+
+        return $updates;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
