9c84ee06c292baf0f4e71eed9bc7693b0543ba27
Ruslan <ya.ruslan2013@mail.ru>
2023-06-15 18:07:34 +0300

    Test sdk

--- app/routes/web.php
+++ app/routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function() {
 });
 
 Route::get('/setwebhook', function () {
-    $response = Telegram::setWebhook(['url' => env("TELEGRAM_WEBHOOK_URL")]);
+    $response = Telegram::setWebhook(['url' => 'https://green-soft.pro/api/{env("TELEGRAM_BOT_TOKEN")}/webhook']);
     dd($response);
 });
 
