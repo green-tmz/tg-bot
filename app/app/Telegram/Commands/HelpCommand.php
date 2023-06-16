97f9c437b2edf3a38dc46d17165af40ce5ef4b4d
Ruslan <ya.ruslan2013@mail.ru>
2023-06-16 16:04:52 +0300

    Test sdk

--- app/app/Telegram/Commands/HelpCommand.php
+++ app/app/Telegram/Commands/HelpCommand.php
@@ -28,6 +28,9 @@ class HelpCommand extends Command
         $commands = $this->getTelegram()->getCommands();
 
         $text = "<b>Список доступных команд:</b>".PHP_EOL;
+        $text .= "/help - Основные команды".PHP_EOL;
+        $text .= "/about - Описание бота".PHP_EOL;
+        $text .= "/start - Основное меню".PHP_EOL;
 
         $this->replyWithMessage([
             'text' => $text,
