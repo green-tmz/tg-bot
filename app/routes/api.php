6572de237044d39a9d379dd634cc9ccc7790c2df
Ruslan <ya.ruslan2013@mail.ru>
2023-06-16 11:09:25 +0300

    Test sdk

--- app/routes/api.php
+++ app/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\TestSdkController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
@@ -20,8 +21,15 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/{env("TELEGRAM_BOT_TOKEN")}/webhook', function () {
-    return Telegram::commandsHandler(true);
+Route::post('/{env("TELEGRAM_BOT_TOKEN")}/webhook', function ($request) {
+    Log::info("ok123");
+    Log::info($request);
+    // $commands = [...];
+    // $telegram->addCommands($commands);
+    // $commandsHandler = $telegram->commandsHandler(true);
+    // //$command = "yourCommand" for example, $arguments = array of something
+    // $res = $telegram->getCommandBus()->execute($command, $arguments, $commandsHandler);
+    // return Telegram::commandsHandler(true);
 });
 
 Route::resource('tests', TestSdkController::class);
