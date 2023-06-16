4ed160730470d07f0c5d506d136df485f4840a96
Ruslan <ya.ruslan2013@mail.ru>
2023-06-16 13:06:12 +0300

    Test sdk

--- app/routes/api.php
+++ app/routes/api.php
@@ -36,7 +36,7 @@ Route::post('/{env("TELEGRAM_BOT_TOKEN")}/webhook', function () {
     Log::info("-------");
 
     foreach ($updates as $update) {
-        Log::info($update->message->text);
+        Log::info($update['message']['text']);
     }
 
     // if ($updates->isType('callback_query')) {
