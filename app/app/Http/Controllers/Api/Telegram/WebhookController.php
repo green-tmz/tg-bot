0d082a69757dfc2268b35bf469159b033d388d19
Ruslan <ya.ruslan2013@mail.ru>
2023-06-16 14:33:29 +0300

    Test sdk

---
+++ app/app/Http/Controllers/Api/Telegram/WebhookController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api\Telegram;
+
+use App\Http\Controllers\Controller;
+use App\Telegram\Commands\AboutCommand;
+use Telegram\Bot\Laravel\Facades\Telegram;
+use Illuminate\Support\Facades\Log;
+
+class WebhookController extends Controller
+{
+    public function index()
+    {
+        // Telegram::addCommands($commands);
+        $commandsHandler = Telegram::commandsHandler(true);
+        $updates = Telegram::getWebhookUpdates();
+        $res1 = json_decode($updates, true);
+
+        if (isset($updates->message->entities) && ($updates->message->entities[0]->type == 'bot_command')) {
+            $commandName = ucfirst(substr($updates->message->text, 1));
+            $commandClass = 'App\\Telegram\\Commands\\'.$commandName.'Command';
+
+            if (class_exists($commandClass)) {
+                Log::info("Команда найдена: ".$commandClass);
+            } else {
+                Log::info("Нет такой команды");
+            }
+        } else {
+            Log::info("Text: ". $updates->message->text);
+        }
+
+
+        // //$command = "yourCommand" for example, $arguments = array of something
+        // $res = Telegram::getCommandBus()->execute($res1['message']['text'], [], $commandsHandler);
+        // Log::info("---2---");
+        // Log::info($res);
+        // Log::info("-------");
+        // // return Telegram::commandsHandler(true);
+    }
+}
--- app/routes/api.php
+++ app/routes/api.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\TestSdkController;
-use App\Telegram\Commands\AboutCommand;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
-use Telegram\Bot\Laravel\Facades\Telegram;
+use App\Http\Controllers\Api\TestSdkController;
+use App\Http\Controllers\Api\Telegram\WebhookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,33 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/{env("TELEGRAM_BOT_TOKEN")}/webhook', function () {
-
-    // Telegram::addCommands($commands);
-    $commandsHandler = Telegram::commandsHandler(true);
-    $updates = Telegram::getWebhookUpdates();
-    $res1 = json_decode($updates, true);
-
-    if (isset($updates->message->entities) && ($updates->message->entities[0]->type == 'bot_command')) {
-        Log::info("Command: ". $updates->message->text);
-        $command = ucfirst(substr($updates->message->text, 1));
-        $commands = [
-            '/about' => AboutCommand::class
-        ];
-
-        Log::info("Commands: ". $commands['/about']);
-        Log::info("Commands: ". $command);
-    } else {
-        Log::info("Text: ". $updates->message->text);
-    }
-
-
-    // //$command = "yourCommand" for example, $arguments = array of something
-    // $res = Telegram::getCommandBus()->execute($res1['message']['text'], [], $commandsHandler);
-    // Log::info("---2---");
-    // Log::info($res);
-    // Log::info("-------");
-    // // return Telegram::commandsHandler(true);
-});
+Route::post('/{env("TELEGRAM_BOT_TOKEN")}/webhook', [WebhookController::class, 'index']);
 
 Route::resource('tests', TestSdkController::class);
