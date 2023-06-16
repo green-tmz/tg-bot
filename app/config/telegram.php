cd5274bb219a9d1e30821a15f8aa64799dfeb946
Ruslan <ya.ruslan2013@mail.ru>
2023-06-16 14:59:13 +0300

    Test sdk

--- app/config/telegram.php
+++ app/config/telegram.php
@@ -37,10 +37,9 @@ return [
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
             'commands'            => [
-                // Acme\Project\Commands\MyTelegramBot\BotCommand::class,
-                // App\Telegram\Commands\HelpCommand::class,
-                // App\Telegram\Commands\StartCommand::class,
-                // App\Telegram\Commands\AboutCommand::class,
+                App\Telegram\Commands\HelpCommand::class,
+                App\Telegram\Commands\StartCommand::class,
+                App\Telegram\Commands\AboutCommand::class,
             ],
         ],
 
