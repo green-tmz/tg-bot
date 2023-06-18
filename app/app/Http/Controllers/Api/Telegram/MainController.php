34cc542be88c06a0ebba7962f3c11578b43b594e
Ruslan <ya.ruslan2013@mail.ru>
2023-06-18 15:56:06 +0300

    Test m3u

--- app/app/Http/Controllers/Api/Telegram/MainController.php
+++ app/app/Http/Controllers/Api/Telegram/MainController.php
@@ -36,7 +36,7 @@ class MainController extends Controller
 
         $response = Telegram::sendAudio([
             'chat_id' => 'CHAT_ID',
-            'photo' => \Telegram\Bot\FileUpload\InputFile::create("http://stream.delovaya-volna.ru/radio/DACHA_TUIMAZY.MP3"),
+            'audio' => \Telegram\Bot\FileUpload\InputFile::create("http://stream.delovaya-volna.ru/radio/DACHA_TUIMAZY.MP3"),
             'caption' => ''
         ]);
 
