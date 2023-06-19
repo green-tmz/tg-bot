57534d7d870c6b6de296867efb30f771bf675ce7
Ruslan <ya.ruslan2013@mail.ru>
2023-06-19 08:55:41 +0300

    Start menu

--- app/app/Http/Controllers/Api/Telegram/MainController.php
+++ app/app/Http/Controllers/Api/Telegram/MainController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers\Api\Telegram;
 
-use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
-use Telegram\Bot\FileUpload\InputFile;
+use Telegram\Bot\Keyboard\Keyboard;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class MainController extends Controller
@@ -12,33 +11,22 @@ class MainController extends Controller
     public static function commandsHandler()
     {
         $updates = Telegram::getWebhookUpdates();
-        $text = "Привет ".$updates->message->from->first_name.PHP_EOL;
-        // $text .= "/help - Основные команды".PHP_EOL;
+        $firstButton = Keyboard::inlineButton(['text' => 'Test', 'callback_data' => 'data']);
+        $secondButton = Keyboard::inlineButton(['text' => 'Btn 2', 'callback_data' => 'data_from_btn2']);
+
+        $keyboard = Telegram::replyKeyboardMarkup([])->inline()
+            ->row($firstButton)
+            ->row($secondButton);
+
+        $text = "Добро пожаловать в бот GreenSoftPro".PHP_EOL.PHP_EOL;
+        $text .= "Выберите категорию:".PHP_EOL;
         // $text .= "/about - Описание бота".PHP_EOL;
         // $text .= "/start - Основное меню".PHP_EOL;
 
-        // Telegram::sendMessage([
-        //     'chat_id' => $updates->message->from->id,
-        //     'text' => $text,
-        //     'parse_mode' => 'html'
-        // ]);
-
-        // Log::info(Telegram::getMe());
-
-        // InputFile::create('http://stream.delovaya-volna.ru/radio/DACHA_TUIMAZY.MP3', 'DACHA_TUIMAZY');
-
-        // $res = Telegram::sendAudio([
-        //     'chat_id' => $updates->message->from->id,
-        //     'audio' => 'http://stream.delovaya-volna.ru/radio/DACHA_TUIMAZY.MP3',
-        //     // 'parse_mode' => 'html'
-        // ]);
-
-        $response = Telegram::sendAudio([
-            'chat_id' => 'CHAT_ID',
-            'audio' => InputFile::create("http://stream.delovaya-volna.ru/radio/HUMOR_FM_TUIMAZY.MP3"),
-            'caption' => ''
+        Telegram::sendMessage([
+            'text' => $text,
+            'parse_mode' => 'html',
+            'reply_markup' => $keyboard
         ]);
-
-        Log::info($response);
     }
 }
--- app/app/Telegram/Commands/StartCommand.php
+++ app/app/Telegram/Commands/StartCommand.php
@@ -2,9 +2,8 @@
 
 namespace App\Telegram\Commands;
 
+use App\Http\Controllers\Api\Telegram\MainController;
 use Telegram\Bot\Commands\Command;
-use Telegram\Bot\Keyboard\Keyboard;
-use Telegram\Bot\Laravel\Facades\Telegram;
 
 class StartCommand extends Command
 {
@@ -13,22 +12,6 @@ class StartCommand extends Command
 
     public function handle()
     {
-        $firstButton = Keyboard::inlineButton(['text' => 'Test', 'callback_data' => 'data']);
-        $secondButton = Keyboard::inlineButton(['text' => 'Btn 2', 'callback_data' => 'data_from_btn2']);
-
-        $keyboard = Telegram::replyKeyboardMarkup([])->inline()
-            ->row($firstButton)
-            ->row($secondButton);
-
-        $text = "Добро пожаловать в бот GreenSoftPro".PHP_EOL.PHP_EOL;
-        $text .= "Выберите категорию:".PHP_EOL;
-        // $text .= "/about - Описание бота".PHP_EOL;
-        // $text .= "/start - Основное меню".PHP_EOL;
-
-        $this->replyWithMessage([
-            'text' => $text,
-            'parse_mode' => 'html',
-            'reply_markup' => $keyboard
-        ]);
+        MainController::commandsHandler();
     }
 }
