e770b6d02b8985206b26286e2a1904936f1cf918
Ruslan <ya.ruslan2013@mail.ru>
2023-06-19 09:02:33 +0300

    Start menu

--- app/app/Http/Controllers/Api/Telegram/MainController.php
+++ app/app/Http/Controllers/Api/Telegram/MainController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Api\Telegram;
 
 use App\Http\Controllers\Controller;
-use Telegram\Bot\Keyboard\Keyboard;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class MainController extends Controller
@@ -11,22 +10,15 @@ class MainController extends Controller
     public static function commandsHandler()
     {
         $updates = Telegram::getWebhookUpdates();
-        $firstButton = Keyboard::inlineButton(['text' => 'Test', 'callback_data' => 'data']);
-        $secondButton = Keyboard::inlineButton(['text' => 'Btn 2', 'callback_data' => 'data_from_btn2']);
-
-        $keyboard = Telegram::replyKeyboardMarkup([])->inline()
-            ->row($firstButton)
-            ->row($secondButton);
-
-        $text = "Добро пожаловать в бот GreenSoftPro".PHP_EOL.PHP_EOL;
-        $text .= "Выберите категорию:".PHP_EOL;
+        $text = "Привет ".$updates->message->from->first_name.PHP_EOL;
+        // $text .= "/help - Основные команды".PHP_EOL;
         // $text .= "/about - Описание бота".PHP_EOL;
         // $text .= "/start - Основное меню".PHP_EOL;
 
         Telegram::sendMessage([
+            'chat_id' => $updates->message->from->id,
             'text' => $text,
-            'parse_mode' => 'html',
-            'reply_markup' => $keyboard
+            'parse_mode' => 'html'
         ]);
     }
 }
