093fabe1300932a37085c44f32403e035fbad29b
Ruslan <ya.ruslan2013@mail.ru>
2023-06-19 07:41:09 +0300

    Start menu

--- app/app/Telegram/Commands/StartCommand.php
+++ app/app/Telegram/Commands/StartCommand.php
@@ -12,7 +12,7 @@ class StartCommand extends Command
     public function handle()
     {
         $response = $this->getUpdate();
-        $text = "Добро пожаловать в бот GreenSoftPro".PHP_EOL;
+        $text = "Добро пожаловать в бот GreenSoftPro".PHP_EOL.PHP_EOL;
         $text .= "Выберите категорию:".PHP_EOL;
         // $text .= "/about - Описание бота".PHP_EOL;
         // $text .= "/start - Основное меню".PHP_EOL;
