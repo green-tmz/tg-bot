fb27846b11f2cb42faef12d7fc9cd1ea3361691d
Ruslan <ya.ruslan2013@mail.ru>
2023-06-19 09:07:06 +0300

    Start menu

--- app/app/Http/Controllers/Api/Telegram/MainController.php
+++ app/app/Http/Controllers/Api/Telegram/MainController.php
@@ -15,10 +15,22 @@ class MainController extends Controller
         // $text .= "/about - Описание бота".PHP_EOL;
         // $text .= "/start - Основное меню".PHP_EOL;
 
+        $keyboard = array(
+            array(
+               array('text'=>':like:','callback_data'=>'{"action":"like","count":0,"text":":like:"}'),
+               array('text'=>':joy:','callback_data'=>'{"action":"joy","count":0,"text":":joy:"}'),
+               array('text'=>':hushed:','callback_data'=>'{"action":"hushed","count":0,"text":":hushed:"}'),
+               array('text'=>':cry:','callback_data'=>'{"action":"cry","count":0,"text":":cry:"}'),
+               array('text'=>':rage:','callback_data'=>'{"action":"rage","count":0,"text":":rage:"}')
+            )
+        );
+
         Telegram::sendMessage([
             'chat_id' => $updates->message->from->id,
             'text' => $text,
-            'parse_mode' => 'html'
+            'parse_mode' => 'html',
+            'disable_web_page_preview' => false,
+            'reply_markup' => json_encode(array('inline_keyboard' => $keyboard))
         ]);
     }
 }
