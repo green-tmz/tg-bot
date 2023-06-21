720d5f02bd6bda684fd19af73a5b294f559bf808
Ruslan <ya.ruslan2013@mail.ru>
2023-06-21 09:38:23 +0300

    Start menu

--- app/app/Http/Controllers/Api/Telegram/MainController.php
+++ app/app/Http/Controllers/Api/Telegram/MainController.php
@@ -124,8 +124,19 @@ class MainController extends Controller
 
     public function setName()
     {
-        Telegram::setMyName([
-            'name' => 'Test',
-        ]);
+        print_r($this->sendTelegram('setMyName', 'Test'), true);
+    }
+
+    function sendTelegram($method, $response)
+    {
+        $ch = curl_init('https://api.telegram.org/bot'.env("TELEGRAM_BOT_TOKEN").'/'.$method);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $response);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $res = curl_exec($ch);
+        curl_close($ch);
+
+        return $res;
     }
 }
